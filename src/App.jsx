a22724c17642752d1222031505d2bdfdbdd5d0ae
OpheliaDanso <mayaafreh@gmail.com>
2024-05-06 12:42:49 +0200

    landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import LandingPage from "./Components/Landingpage.jsx";
+
 function App() {
+  const [getPokemon, setGetPokemon] = useState([]);
+
+  const URL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        setGetPokemon(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   return (
     <>
       <h1>Hallo world</h1>
+      <LandingPage />
     </>
   );
 }
