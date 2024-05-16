cdf1dba75ffcb83d111e019c89cb5a20b4c6790d
OpheliaDanso <mayaafreh@gmail.com>
2024-05-16 17:52:49 +0200

    remove jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import PokemonList from "./Components/PokemonList";
 import SinglePokemon from "./Components/SinglePokemon";
 import AboutUs from "./Components/AboutUs.jsx";
 import Footer from "./Components/Footer";
-import FightersPage from "./Components/FightersPage.jsx";
+import FightersPage from "./Components/FightersPage";
 import LandingPage from "./Components/LandingPage.jsx"; // Move this import here
-import "./index.css";
 import Image from "./assets/images/pokemonNew.webp";
 import LeaderBoard from "./Components/LeaderBoard.jsx";
 import { useState } from "react";
+import "./index.css";
 
 function App() {
   const [selectedPokemon, setSelectedPokemon] = useState(null);
